3868ef4f98a9da04bef7c914407677f3177f2310
NoName <buybackbrain.genetic@gmail.com>
2023-03-14 10:09:24 +0300

    fix

--- Home 1/Task 8.py
+++ Home 1/Task 8.py
@@ -2,9 +2,7 @@ height = int(input("Введите высоту шоколадки"))
 weight = int(input("Ведите ширину шоколадки"))
 num = int(input("Сколько долек нужно получить"))
 
-if(height*weight)%num == 0:
+if (height * weight) % num == 0 or num % height == 0 or num % weight == 0:
     print("это возможно")
 else:
     print("это невозможно")
-
-
